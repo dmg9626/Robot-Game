dd5d3ba04b2092b47ecafaaf8ab30cb53f800314
Drew Graham <Drew Graham>
2017-11-23 17:54:48 -0500

    initial commit

---
+++ Assets/Scripts/OldPlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OldPlayerController : MonoBehaviour 
+{
+    public enum Direction {
+        Up = 1,
+        Right,
+        Down,
+        Left
+    }
+
+    private Animator animator;
+    private Direction currentDirection;
+    public float moveSpeed;
+
+    void Start () 
+    { 
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update () 
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        ManageMovement(h, v);
+    }
+
+    void ManageMovement(float horizontal,float vertical) 
+    {
+        if (horizontal != 0f || vertical != 0f) 
+        {
+            animator.SetBool("Moving", true); 
+            animateWalk(horizontal, vertical);
+        } 
+        else 
+        {
+            animator.SetBool("Moving", false);
+        }
+
+        Vector3 movement = new Vector3 (horizontal * moveSpeed, vertical * moveSpeed, 0);
+        gameObject.GetComponent<Rigidbody2D>().velocity = movement;
+    }
+
+    void animateWalk(float horizontal,float vertical) 
+    {
+        currentDirection = (Direction)animator.GetInteger("Direction");
+
+        if ((vertical > 0) && (vertical > horizontal)) {
+            animator.SetInteger("Direction", (int)Direction.Up); // up
+        }
+        else if ((horizontal > 0) && (vertical < horizontal)) {
+            animator.SetInteger("Direction", (int)Direction.Right); // right
+        }
+        else if ((vertical < 0) && (vertical < horizontal)) {
+            animator.SetInteger("Direction", (int)Direction.Down); // down
+        }
+        else if ((horizontal < 0 ) && (vertical > horizontal)) {
+            animator.SetInteger("Direction", (int)Direction.Left); // left
+        }
+    
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	public float time;
+	public float speed;
+	public Projectile() {
+		
+	}
+
+	public void SetTrajectory(Vector2 trajectory) {
+		trajectory.x *= speed;
+		trajectory.y *= speed;
+		GetComponent<Rigidbody2D>().velocity = trajectory;
+	}
+	
+	void Update () {
+		if(time > 4) {
+			Destroy(gameObject);
+		}
+		else {
+			time += Time.deltaTime;
+		}
+	}
+}
