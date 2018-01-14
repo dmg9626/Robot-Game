213985d93ee5ae9a5ce282ba57ffda1c9a5aee86
Drew Graham <Drew Graham>
2018-01-14 02:43:45 -0500

    configured LogHelper system, created PhysicsLogger

---
+++ DomesTech/Assets/Scripts/Utility/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public bool logPhysics;
+
+	void Start()
+	{
+		LogPhysics = new LogHelper();
+		InitializeLoggers();
+	}
+
+	private void InitializeLoggers()
+	{
+		LogPhysics.SetLogging(logPhysics);
+	}
+	/// <summary>
+	/// Physics logger
+	/// </summary>
+	public static LogHelper LogPhysics;
+}	
