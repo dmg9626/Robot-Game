c9f4ae4ee5fde5467f84cdd68502d6cca9d11eac
Drew Graham <dmg346@drexel.edu>
2018-07-01 19:08:18 -0400

    projectile now faces direction it is shot

--- DomesTech/Assets/Scripts/Projectile.cs
+++ DomesTech/Assets/Scripts/Projectile.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-	public float lifeSpan;
-	private float time = 0;
-	public float speed;
-	public Projectile() {
-		
-	}
 
-	public void SetTrajectory(Vector2 trajectory) {
-		GetComponent<Rigidbody2D>().velocity = trajectory * speed;
-		Debug.Log("Trajectory: " + trajectory);
-	}
+    /// <summary>
+    /// Time to wait for collision before disappearing
+    /// </summary>
+    public float lifeSpan;
+
+    /// <summary>
+    /// Time counter
+    /// </summary>
+    private float time = 0;
+
+    /// <summary>
+    /// Projectile speed
+    /// </summary>
+    public float speed;
 	
 	void Update () {
 		if(time > lifeSpan) {
@@ -23,4 +27,23 @@ public class Projectile : MonoBehaviour {
 			time += Time.deltaTime;
 		}
 	}
+
+    /// <summary>
+    /// Shoots projectile in given direction
+    /// </summary>
+    /// <param name="vector">Vector</param>
+    public void Shoot(Vector2 vector)
+    {
+        // Set velocity
+        GetComponent<Rigidbody2D>().velocity = vector * speed;
+        GameController.LogPhysics.Log("Trajectory: " + vector);
+
+        // Set rotation
+        if (!DirectionHelper.IsVertical(DirectionHelper.VectorToDirection(vector)))
+        {
+            Quaternion rotation = transform.rotation;
+            rotation.eulerAngles = new Vector3(0, 0, 90F);
+            transform.rotation = rotation;
+        }
+    }
 }
